8323627fe6d25955199fe9311eca6d532371f5cb
Puspak <69215834+phycoding@users.noreply.github.com>
2021-07-15 23:08:09 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,34 +8,34 @@ from keras.preprocessing.image import img_to_array
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
+
+# You can load your model by training or by using the file location of downloaded model
+
 my_model = keras.models.load_model(r'C:\Users\ASUS\stapp\my_model\my_model.h5')
 lookup = ('anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise', 'neutral')
 
-Anger_dict = {'Bangiri Mata':'C:/Users/ASUS/stapp/songs/Anger/Bangirimata.mp3','Churake dilmera':"C:/Users/ASUS/stapp/songs/Anger/churake.mp3"}
 def image_input():
     content_file=None
     if st.sidebar.checkbox('Upload'):
         content_file = st.sidebar.file_uploader("Choose a Content Image", type=["png", "jpg", "jpeg"])
     else:
       st.markdown('Please select **Upload button**.')
-        #content_name = st.sidebar.selectbox("Choose the content images:", content_images_name)
-        #content_file = content_images_dict[content_name]
-
+       
     if content_file is not None:
         content = detect_face(content_file)
+        x = detect_emotion(content)
         #content = np.array(content) #pil to cv
         #content = cv2.cvtColor(content, cv2.COLOR_RGB2BGR)
     else:
         st.warning("Upload an Image OR Untick the Upload Button)")
         st.stop()
-    return st.image(content,caption = "Ho gaya bhaya tera face detection")
+    return st.image(content,caption = "You are looking like to have in: %s" %x)
 def detect_face(images):
   try:
     images = PIL.Image.open(images)
     new_img = np.array(images.convert('RGB'))
     faces = face_cascade.detectMultiScale(new_img, 1.1, 4)
     for (x,y,w,h) in faces:
-      #gray = cv2.cvtColor(new_img,cv2.COLOR_BGR2GRAY)
       cv2.rectangle(new_img,(x,y),(x+w,y+h),(255,0,0),7)
     return new_img
   except:
@@ -44,7 +44,6 @@ def detect_face(images):
     images.astype(np.float32)
     faces = face_cascade.detectMultiScale(images, 1.1, 4)
     for (x,y,w,h) in faces:
-      #gray = cv2.cvtColor(new_img,cv2.COLOR_BGR2GRAY)
       cv2.rectangle(images,(x,y),(x+w,y+h),(255,0,0),7)
     return images
 def webcam_input():
@@ -64,22 +63,14 @@ def webcam_input():
        orig = detect_face(orig)
        orig1 = cv2.cvtColor(orig,cv2.COLOR_BGR2GRAY)
        x = detect_emotion(orig)
-       #orig1 = cv2.resize(orig1,(1,48,48,1),interpolation=cv2.INTER_AREA)
-       #pred = my_model.predict(orig1)
-        #orig = imutils.resize(frame, width=300)
-        #frame = imutils.resize(frame, width=WIDTH)
-        #target = detect_face_video(orig)
-       
        FRAME_WINDOW.image(orig)
        SIDE_WINDOW.image(frame.copy())
        status_text.text('You are in: %s'%x)
        print(x)
-       #audio = st.sidebar.selectbox("Looks like angry choose the song to boost :",Anger_dict)
        
     else:
         st.warning("NOTE: Streamlit currently doesn't support webcam. So to use this, clone this repo and run it on local server.")
         st.warning('Stopped')
-    #st.audio(Anger_dict['Bangiri Mata'])
 def detect_emotion(images):
   try:
     images = PIL.Image.open(images)
@@ -100,7 +91,6 @@ def detect_emotion(images):
     images = np.array(images)
     images.astype(np.float32)
     faces = face_cascade.detectMultiScale(images, 1.1, 4)
-    #status_text = st.text("loading..")
     status_text = st.empty()
     for (x,y,w,h) in faces:
       gray = cv2.cvtColor(images,cv2.COLOR_BGR2GRAY)
@@ -121,7 +111,6 @@ def main():
   method = st.sidebar.radio('Go To ->', options=['Image','Webcam'],key = 'amaifkajlfj')
   st.sidebar.header('Options')
 
-  #audio = st.sidebar.selectbox("Choose the style model: ", Anger_dict)
 
   if method == 'Image':
     image_input()
